e7ef113cdeffdb850ac51901df8d653a71bd6e45
Elias.Hovdenes <elias.hovdenes@student.uib.no>
2024-10-13 14:07:28 +0200

    Now the pokemon data looks a little better

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './App.css'; 
 
 function App() {
   const [pokemonData, setPokemonData] = useState(null);
@@ -19,35 +20,21 @@ function App() {
       })
       .catch(error => {
         console.error('Error fetching data:', error);
-        console.log('API URL:', apiUrl);
-        console.log('API URL:', process.env.REACT_APP_API_URL);
-        if (error.response) {
-          // The request was made and the server responded with a status code
-          console.log('Response error data:', error.response.data);
-          console.log('Response error status:', error.response.status);
-          console.log('Response error headers:', error.response.headers);
-        } else if (error.request) {
-          // The request was made but no response was received
-          console.log('No response received:', error.request);
-        } else {
-          // Something happened in setting up the request that triggered an Error
-          console.log('Error setting up request:', error.message);
-        }
       });
   }, [apiUrl]);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Random Pokemon</h1>
-          {pokemonData ? (
-            <div>
-              <p>Pokémon: {pokemonData.name}</p>
-              <p>Types: {pokemonData.types.join(', ')}</p>
-            </div>
-          ) : (
-            <p>Loading...</p>
-          )}
+    <div className="container">
+      <header className="header">
+        <h1 className="title">Random Pokemon</h1>
+        {pokemonData ? (
+          <div className="pokemon-container">
+            <p className="pokemon-text">Pokémon: {pokemonData.name}</p>
+            <p className="pokemon-text">Types: {pokemonData.types.join(', ')}</p>
+          </div>
+        ) : (
+          <p className="loading-text">Loading...</p>
+        )}
       </header>
     </div>
   );
